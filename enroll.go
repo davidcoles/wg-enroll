d9205b5344256a8c8839f4508ff01bc5d1acc486
David Coles <david.coles@global.com>
2025-01-13 10:24:12 +0000

    create tokens less wrongly ...

--- enroll.go
+++ enroll.go
@@ -18,29 +18,40 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	"golang.org/x/crypto/curve25519"
-	"golang.org/x/crypto/ssh"
 )
 
-// TODO: add timestamp
 type DeviceInfo struct {
 	MAC       string            `json:"mac,omitempty"`
 	WireGuard string            `json:"wireguard,omitempty"`
-	Lshw      string            `json:"lshw,omitempty"`  // lshw -json
-	Lspci     string            `json:"lspci,omitempty"` // lspci -vvnn
+	Lshw      string            `json:"lshw,omitempty"`  // base64url(lshw -json)
+	Lspci     string            `json:"lspci,omitempty"` // base64url(lspci -vvnn)
 	SSH       map[string]string `json:"ssh,omitempty"`   // $(cat /etc/ssh/*.pub | sed 's/ /:/g' | cut -d: -f1-2)
+	Timestamp int64             `json:"timestamp,omitempty"`
 }
 
-// enrol <mac> <configfile> <lshw.json>
+func (d *DeviceInfo) base64() (string, error) {
+	payload, err := json.Marshal(d)
+
+	if err != nil {
+		return "", err
+	}
+
+	return base64.RawURLEncoding.EncodeToString(payload), nil
+}
+
+func (d *DeviceInfo) marshal() ([]byte, error) { return json.Marshal(d) }
+
+// enrol [-lshw lshw.json] [-lspci lspci.txt] <mac> <configfile> [ssh:key]...
 // systemctl enable wg-quick@wg0.service
 
-// -lshw
-// -lspci
-// $(cat /etc/ssh/*.pub | sed 's/ /:/g' | cut -d: -f1-2)
-
+var gen = flag.Bool("gen", false, "generate signing key public/private pair to stdout")
 var lshw = flag.String("lshw", "", "lshw -json")
 var lspci = flag.String("lspci", "", "lspci -vvnn")
+
+// token valididty checker: https://justwebtoken.io/verify/
 
 func main() {
 
@@ -48,6 +59,20 @@ func main() {
 
 	flag.Parse()
 	args := flag.Args()
+
+	if *gen {
+		pub, priv, err := ed25519.GenerateKey(nil)
+		if err != nil {
+			panic(err)
+		}
+		seed := priv.Seed()
+		fmt.Println(
+			// use RawURLEncoding as it is more in keeping with JWT
+			base64.RawURLEncoding.EncodeToString(pub[:]),
+			base64.RawURLEncoding.EncodeToString(seed[:]),
+		)
+		return
+	}
 
 	if m, err := hex.DecodeString(args[0]); err != nil {
 		panic(err)
@@ -58,11 +83,19 @@ func main() {
 	conf := loadConf(args[1])
 	keys := args[2:]
 
-	wgpriv, wgpub := wg()
+	wgpub, wgpriv, err := wg()
 
-	priv, pub := conf.keys()
+	if err != nil {
+		log.Fatalf("Couldn't generate WireGuard keys: %s\n", err)
+	}
 
-	log.Println("Signing with", base64.StdEncoding.EncodeToString(pub[:]))
+	pub, priv, err := conf.keys()
+
+	if err != nil {
+		log.Fatalf("Couldn't load signing key: %s\n", err)
+	}
+
+	log.Println("Signing with", base64.StdEncoding.EncodeToString(pub[:]), wgpriv.encode(), wgpub.encode())
 
 	var device DeviceInfo
 	device.MAC = mac.String()
@@ -70,39 +103,49 @@ func main() {
 	device.Lshw = loadFile(*lshw)
 	device.Lspci = loadFile(*lspci)
 	device.SSH = map[string]string{}
+	device.Timestamp = time.Now().UnixNano() / int64(time.Millisecond) // standard Javascript/JSON timestamp
 
 	re := regexp.MustCompile("^([-a-z0-9]+):([-+_=/a-zA-Z0-9]+)$")
 
 	for _, k := range keys {
 		m := re.FindStringSubmatch(k)
 		if len(m) != 3 {
-			log.Fatal("Funny looking SSH key - ", k)
+			log.Fatal("Funny looking SSH key ... ", k)
 		}
 
 		device.SSH[m[1]] = m[2]
 	}
 
-	payload, err := json.Marshal(device)
+	/*
+		header64 := EdDSA(pub)
+		payload64, err := device.base64()
+
+		if err != nil {
+			log.Fatal("payload marshal", err)
+		}
+
+		preamble := header64 + "." + payload64
+		signature64 := base64.RawURLEncoding.EncodeToString(ed25519.Sign(priv, []byte(preamble)))
+
+		jwt := preamble + "." + signature64
+	*/
+
+	payload, err := device.marshal()
 
 	if err != nil {
 		log.Fatal("payload marshal", err)
 	}
 
-	header := []byte(`{"kty":"OKP","alg":"EdDSA","crv":"Ed25519","x":"` + base64.StdEncoding.EncodeToString(pub[:]) + `"}`)
-	signature := ed25519.Sign(priv, payload)
+	jwt := tokenise(payload, priv)
 
-	jwt := fmt.Sprintf("%s.%s.%s",
-		base64.RawURLEncoding.EncodeToString(header),
-		base64.RawURLEncoding.EncodeToString(payload),
-		base64.RawURLEncoding.EncodeToString(signature),
-	)
+	log.Println(jwt)
+	log.Println("Registrar is", conf.Registrar)
 
-	log.Println("Registrar", conf.Registrar)
 	url := conf.Registrar + mac.String()
 	resp, err := http.Post(url, "application/jose", bytes.NewReader([]byte(jwt)))
 
 	if err != nil {
-		log.Fatal("POST failed", err)
+		log.Fatalf("POST failed: %s\n", err)
 	}
 
 	defer resp.Body.Close()
@@ -110,10 +153,16 @@ func main() {
 	reply, err := ioutil.ReadAll(resp.Body)
 
 	if resp.StatusCode != http.StatusCreated {
-		log.Fatal("StatusCode", resp.StatusCode, string(reply))
+		log.Fatalf("StatusCode: %d - %s\n", resp.StatusCode, string(reply))
 	}
 
 	fmt.Println(conf.conf(wgpriv, mac)) // wg0.conf
+}
+
+func EdDSA(pub ed25519.PublicKey) string {
+	pub64 := base64.RawURLEncoding.EncodeToString(pub[:])
+	header := []byte(`{"kty":"OKP","alg":"EdDSA","crv":"Ed25519","x":"` + pub64 + `"}`)
+	return base64.RawURLEncoding.EncodeToString(header)
 }
 
 func loadFile(file string) string {
@@ -163,43 +212,6 @@ func loadConf(file string) (w wgconf) {
 	return
 }
 
-func loadSSHKey(file string) (*ed25519.PrivateKey, ed25519.PublicKey) {
-
-	f, err := os.Open(file)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer f.Close()
-
-	b, err := ioutil.ReadAll(f)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	i, err := ssh.ParseRawPrivateKey(b)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	priv, ok := i.(*ed25519.PrivateKey)
-
-	if !ok {
-		log.Fatal("Not an ed25519 key")
-	}
-
-	pub, ok := priv.Public().(ed25519.PublicKey)
-
-	if !ok {
-		log.Fatal("Not an ed25519 key")
-	}
-
-	return priv, pub
-}
-
 type Key [32]byte
 
 func (k *Key) encode() string {
@@ -210,65 +222,33 @@ func (k Key) String() string {
 	return base64.StdEncoding.EncodeToString(k[:])
 }
 
-func wg() (Key, Key) {
+func wg() (pub Key, priv Key, err error) {
+	// follow same process as wireguard-tools/src/genkey.c
 
-	priv, err := Genkey()
+	if n, err := rand.Read(priv[:]); err != nil {
+		return pub, priv, err
+	} else if n != 32 {
+		return pub, priv, errors.New("Failed to read 32 bytes from random source")
+	}
+
+	// clamp private key (yeah, me neither ...️) as per: https://cr.yp.to/ecdh.html
+	priv[0] &= 248
+	priv[31] &= 127
+	priv[31] |= 64
+
+	pub_, err := curve25519.X25519(priv[:], curve25519.Basepoint)
 
 	if err != nil {
-		panic(err.Error())
+		return pub, priv, err
 	}
 
-	pub, err := Pubkey(priv)
-
-	if err != nil {
-		panic(err.Error())
+	if len(pub_) != 32 {
+		return pub, priv, errors.New("Key is not 32 bytes long")
 	}
 
-	return priv, pub
+	copy(pub[:], pub_[:])
 
-}
-
-func Genkey() (Key, error) {
-	var key [32]byte
-
-	n, err := rand.Read(key[:])
-
-	if err != nil {
-		return key, err
-	}
-
-	if n != 32 {
-		return key, errors.New("Failed to read 32 bytes fron random source")
-	}
-
-	// https://cr.yp.to/ecdh.html
-
-	key[0] &= 248
-	key[30] &= 127
-	key[31] |= 64
-
-	return key, nil
-}
-
-func Pubkey(private [32]byte) ([32]byte, error) {
-
-	var public [32]byte
-
-	curve25519.ScalarBaseMult(&public, &private)
-
-	x, err := curve25519.X25519(private[:], curve25519.Basepoint)
-
-	if err != nil {
-		return public, err
-	}
-
-	if len(x) != 32 {
-		return public, errors.New("Key is not 32 bytes long")
-	}
-
-	copy(public[:], x[:])
-
-	return public, nil
+	return pub, priv, nil
 }
 
 type MAC [6]byte
@@ -286,15 +266,16 @@ type wgconf struct {
 	Registrar           string
 }
 
-func (c *wgconf) keys() (ed25519.PrivateKey, ed25519.PublicKey) {
-	seed, err := base64.StdEncoding.DecodeString(c.SigningKey)
+// func (c *wgconf) keys() (ed25519.PrivateKey, ed25519.PublicKey) {
+func (c *wgconf) keys() (ed25519.PublicKey, ed25519.PrivateKey, error) {
+	seed, err := base64.RawURLEncoding.DecodeString(c.SigningKey) // use RawURLEncoding as it is more in keeping with JWT
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, err
 	}
 
 	priv := ed25519.NewKeyFromSeed(seed)
-	return priv, priv.Public().(ed25519.PublicKey)
+	return priv.Public().(ed25519.PublicKey), priv, nil
 }
 
 func (c *wgconf) conf(priv Key, mac MAC) string {
@@ -320,4 +301,15 @@ func (c *wgconf) conf(priv Key, mac MAC) string {
 	s = append(s, "PersistentKeepalive = "+pka)
 	s = append(s, "")
 	return strings.Join(s, "\n")
+}
+
+func tokenise(payload []byte, priv ed25519.PrivateKey) string {
+	pub := priv.Public().(ed25519.PublicKey)
+	pub64 := base64.RawURLEncoding.EncodeToString(pub[:])
+	header := []byte(`{"kty":"OKP","alg":"EdDSA","crv":"Ed25519","x":"` + pub64 + `"}`)
+	header64 := base64.RawURLEncoding.EncodeToString(header)
+	payload64 := base64.RawURLEncoding.EncodeToString(payload)
+	preamble := header64 + "." + payload64
+	signature64 := base64.RawURLEncoding.EncodeToString(ed25519.Sign(priv, []byte(preamble)))
+	return preamble + "." + signature64
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.21.1
 
 require golang.org/x/crypto v0.31.0
 
-require golang.org/x/sys v0.28.0 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+)
